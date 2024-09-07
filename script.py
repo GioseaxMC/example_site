b9a04a75ad87babb3511273814e10e326cd5902f
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-07 19:50:45 +0200

    Added Domain registration system

--- script.py
+++ script.py
@@ -1,8 +1,21 @@
-iterations = 0
-random = 0
+spaces1 = " " * 65
+spaces2 = " " * 65
+done = ""
+error = ""
 
+
+def strings_in_string(list_of_strings, main_string):
+    return any(substring in main_string for substring in list_of_strings)
 
 def loop(events, inputs, self):
-    if not self.iterations:
-        self.random = r.randint(0, 100)
-    self.iterations += 1
+
+    if self.strings_in_string(("add_done", "edit_done", "rem_done"), events):
+        if "add_done" in events:
+            self.done, self.error = d.add_domain(inputs["add_domain"], inputs["add_ip"])
+        elif "edit_done" in events:
+            self.done, self.error = d.edit_domain(inputs["edit_key"], inputs["edit_ip"])
+        elif "rem_done" in events:
+            self.done, self.error = d.delete_domain(inputs["rem_key"])
+        
+
+
