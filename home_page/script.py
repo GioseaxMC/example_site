25382609517852190d624c3e60918786e383e29b
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 20:04:16 +0200

    Animated Welcome!

--- home_page/script.py
+++ home_page/script.py
@@ -1,4 +1,7 @@
 search = " - "
+welcome = ""
+message = "Welcome!"
+display = ""
 
 def loop(events, inputs, self):
     if "getlink" in events or inputs.get("link"):
@@ -6,3 +9,8 @@ def loop(events, inputs, self):
     elif "getsearch" in events or inputs.get("search"):
         self.search = browse(self.domains, inputs["search"])
         load_browser(self.search, inputs["search"])
+
+    if not frames() % 3 and len(self.message):
+        self.welcome =+ self.message[0]
+        self.message[1:]
+    self.display = self.welcome + ("_" if not frames() % 30 > 15 else "")
