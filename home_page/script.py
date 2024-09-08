080bb2a38900f0d413edf67a7a3a97c2748645ce
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 15:49:18 +0200

    Added DEMO search

--- home_page/script.py
+++ home_page/script.py
@@ -1,3 +1,5 @@
 def loop(events, inputs, self):
-    if "get" in events or inputs.get("link"):
+    if "getlink" in events or inputs.get("link"):
         load(inputs["link"])
+    elif "getsearch" in events or inputs.get("search"):
+        load(browse(inputs["search"])[0]["ip"])
