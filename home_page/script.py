8b36efe7b8c05d1a108370425f51a7a45be4d62a
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 18:24:42 +0200

    Added Browsing page

--- home_page/script.py
+++ home_page/script.py
@@ -1,5 +1,8 @@
+search = " - "
+
 def loop(events, inputs, self):
     if "getlink" in events or inputs.get("link"):
         load(inputs["link"])
     elif "getsearch" in events or inputs.get("search"):
-        load(browse(self.domains, inputs["search"])[0]["ip"])
+        self.search = browse(self.domains, inputs["search"])
+        load_browser(self.search, inputs["search"])
