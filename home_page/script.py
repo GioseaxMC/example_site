e33c85c009c3abe0e58a331c2497f383b51755db
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 15:56:19 +0200

    Fixed script

--- home_page/script.py
+++ home_page/script.py
@@ -2,4 +2,4 @@ def loop(events, inputs, self):
     if "getlink" in events or inputs.get("link"):
         load(inputs["link"])
     elif "getsearch" in events or inputs.get("search"):
-        load(browse(inputs["search"], domains)[0]["ip"])
+        load(browse(domains, inputs["search"])[0]["ip"])
