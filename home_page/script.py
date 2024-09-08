b6f69b0f0560fff1299ec001c83f39758089d795
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 16:03:32 +0200

    Fixed domains

--- home_page/script.py
+++ home_page/script.py
@@ -2,4 +2,4 @@ def loop(events, inputs, self):
     if "getlink" in events or inputs.get("link"):
         load(inputs["link"])
     elif "getsearch" in events or inputs.get("search"):
-        load(browse(domains, inputs["search"])[0]["ip"])
+        load(browse(self.domains, inputs["search"])[0]["ip"])
