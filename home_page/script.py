50892d4ac629db06d7da43706869be641196c504
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 20:59:09 +0200

    Animated Homepage

--- home_page/script.py
+++ home_page/script.py
@@ -10,8 +10,9 @@ def loop(events, inputs, self):
         self.search = browse(self.domains, inputs["search"])
         load_browser(self.search, inputs["search"])
 
-    if not frames() % 3 and len(self.message):
-        self.welcome =+ self.message[0]
-        self.message[1:]
-    bar = "_" if not frames() % 30 > 15 else ""
+
+    if not frames() % 7 and len(self.message):
+        self.welcome += self.message[0]
+        self.message = self.message[1:]
+    bar = "_" if not frames() % 60 > 30 else ""
     self.display = self.welcome + bar
