b9b43aa00a8e3741d0cdf29ed391dcd7f277f332
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-08 20:05:54 +0200

    Fixed script

--- home_page/script.py
+++ home_page/script.py
@@ -13,4 +13,5 @@ def loop(events, inputs, self):
     if not frames() % 3 and len(self.message):
         self.welcome =+ self.message[0]
         self.message[1:]
-    self.display = self.welcome + ("_" if not frames() % 30 > 15 else "")
+    bar = "_" if not frames() % 30 > 15 else ""
+    self.display = self.welcome + bar
